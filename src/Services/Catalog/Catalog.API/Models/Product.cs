ab5e7d8a57740a1b0b44ed1e65a4f3957f101708
Mohammed Dghoughi <med.dghoughi@gmail.com>
2025-06-24 16:59:25 +0200

    Dev Catalog.API

---
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Catalog.API.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public List<Category> Category { get; set; } = new();
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
